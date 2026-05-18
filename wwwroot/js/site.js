04c405cf223e7275c035257ed152547b60ca8eb3
elijahred23 <elijahred23@gmail.com>
2026-05-17 20:00:12 -0500

    V-16: Added lightbox modal

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,5 +1,50 @@
 ﻿
-$(function() {
+$(function(){
+    let currentImageIndex = 0;
+
+    let $galleryItems = [];
+
+    function updateLightbox(index) 
+    {
+        if(index < 0 || index >= $galleryItems.length) return;
+
+
+        currentImageIndex = index;
+
+        const $item = $($galleryItems[currentImageIndex])
+
+        $('#lightboxImage').attr('src', $item.attr('src'));
+        $('#lightboxCaption').text($item.data('title') || 'No Title');
+    }
+
+
+    $('body').on('click', '.gallery-img', function(e) {
+        e.preventDefault();
+
+        $galleryItems = $('.gallery-img').toArray();
+
+        currentImageIndex = $galleryItems.indexOf(this);
+
+        updateLightbox(currentImageIndex);
+
+        const modal = new bootstrap.Modal(document.getElementById('lightboxModal'));
+        modal.show();
+    });
+
+    $('#prevImage').on('click', function(){
+        updateLightbox((currentImageIndex - 1 + $galleryItems.length) % $galleryItems.length);
+    });
+    $('#nextImage').on('click', function(){
+        updateLightbox((currentImageIndex + 1) % $galleryItems.length);
+    });
+
+    $(document).on('keydown', function(e) {
+        if(!$('#lightboxModal').hasClass('show')) return;
+
+        if(e.key == 'ArrowLeft') $('#prevImage').trigger('click');
+        else if (e.key == 'ArrowRight') $('#nextImage').trigger('click');
+    })
+
     $('.favorite-toggle').on('click', function(e) {
         const $btn = $(this);
 
